2e858b68dd3dbe7d9ba7dc59a18972524dfb03e9
wackyzane <karlsen.per.a@gmail.com>
2024-04-17 23:36:21 -0600

    6.0
    6.0 A'zadi Update is out! @everyone
    We are thrilled to unveil the much-anticipated first major update to our game, marking an extensive journey of development. Introducing the A'zadi region, a captivating locale renowned for its unwavering reverence for the Kon'chi deities and a deep-rooted reluctance towards external interactions. Players will immerse themselves in the intricate dynamics of the empire, navigating the complex societal structures as factions debate the pivotal choice between maintaining isolation or embracing global engagement. Explore the rich tapestry of A'zadi's culture, confront challenging dilemmas, and shape the destiny of a civilization on the brink of profound change. Welcome to a world where tradition clashes with progress, where every decision carries weight, and where the future hangs in the balance.
    
    This update includes:
    A new region
    15 new starting nations
    2 new form-able nations
    The complex voting system of the Amma Narsa
    The brand new religion of Kon'chi
    New Provinces
    Greater localisations
    Many bug fixes

---
+++ common/defines/tir_defines.lua
@@ -0,0 +1,24 @@
+NDefines.NGame.START_DATE = "589.9.17"
+NDefines.NGame.END_DATE = "935.9.17"
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "637.1.1"
+
+NDefines.NDiplomacy.DESIRED_NUM_OF_ELECTORS = 3
+
+NDefines.NGame.GREAT_POWER_FRENZY_START = "845.1.1"
+
+NDefines.NFrontend.CAMERA_LOOKAT_X = 2020.0
+NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 665.0
+NDefines.NFrontend.CAMERA_START_X = 2020.0
+NDefines.NFrontend.CAMERA_START_Y = 800.0
+NDefines.NFrontend.CAMERA_START_Z = 400.0
+NDefines.NFrontend.CAMERA_END_X = 2020.0
+NDefines.NFrontend.CAMERA_END_Y = 800.0
+NDefines.NFrontend.CAMERA_END_Z = 600.0
+
+NDefines.NFrontend.FRONTEND_POS_X = 2020.0
+NDefines.NFrontend.FRONTEND_POS_Y = 900.0
+NDefines.NFrontend.FRONTEND_POS_Z = 665.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 2020.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 665.0
